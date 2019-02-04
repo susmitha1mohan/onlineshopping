04c810cc1a0dd683ea5caffe2e295dd251725742
Susmitha Mohan <sumi.sushmitha@gmail.com>
2019-02-04 18:33:48 +0530

    css added

--- BackendShop/src/main/java/com/sush/BackendShop/config/HibernateConfig.java
+++ BackendShop/src/main/java/com/sush/BackendShop/config/HibernateConfig.java
@@ -20,7 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 public class HibernateConfig {
 	
-	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost/~/onlineshopping";
+	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost/~/shoponline";
 	private final static String DATABASE_DRIVER = "org.h2.Driver";
 	private final static String DATABASE_DIALECT = "org.hibernate.dialect.H2Dialect";
 	private final static String DATABASE_USERNAME = "sa";
